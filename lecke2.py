b77a6d52f27b948e5235d01881f6cc59c762e016
Sz. Levente <96853166+Cob2077@users.noreply.github.com>
2022-11-08 16:17:06 +0100

    Add files via upload

---
+++ lecke2.py
@@ -0,0 +1,8 @@
+'''kiszámolja a megadott rádiuszú kör területét'''
+print("írd be a kör sugarát")
+PI = 3.14
+r = int(input())
+T = (PI*r*r)
+print("Terület =",T)
+K = (2*PI*r)
+print("Kerület =",K)
